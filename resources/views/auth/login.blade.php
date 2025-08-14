4985b8c56ec2fe5d0913bcd7e722e654e0649d02
khanializa32 <khanializa32@gmail.com>
2025-08-14 20:33:19 +0500

    Update login form: Clear pre-filled email and password fields for better security and remove commented-out sections for cleaner code.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,12 +22,12 @@
 
                         <div class="form-group mb-3">
                             <label for="emailaddress" class="form-label">Email address</label>
-                            <input class="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email" name="email" value="zafeer@devop360.com">
+                            <input class="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email" name="email" value="">
                         </div>
 
                         <div class="form-group mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input class="form-control" type="password" required="" id="password" placeholder="Enter your password" name="password" value="devop360">
+                            <input class="form-control" type="password" required="" id="password" placeholder="Enter your password" name="password" value="">
                         </div>
 
                         <div class="form-group d-flex mb-3">
@@ -38,7 +38,7 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 text-end">
-                                <a class='text-muted fs-14' href='{{ route('recover.password') }}'>Forgot password?</a>                             
+                                <a class='text-muted fs-14' href='{{ route('recover.password') }}'>Forgot password?</a>
                             </div>
                         </div>
 
@@ -51,15 +51,15 @@
                         </div>
                     </form>
 
-                    <div class="saprator my-4"><span>or sign in with</span></div>
-        
-                    <div class="text-center text-muted mb-4">
+                    {{-- <div class="saprator my-4"><span>or sign in with</span></div> --}}
+
+                    {{-- <div class="text-center text-muted mb-4">
                         <p class="mb-0">Don't have an account ?<a class='text-primary ms-2 fw-medium' href='{{ route('register') }}'>Sign up</a></p>
-                    </div>
+                    </div> --}}
 
                     <!-- <div class="row">
                         <div class="col-12">
-                            <a class="btn text-dark border fw-normal d-flex align-items-center justify-content-center mb-3"> 
+                            <a class="btn text-dark border fw-normal d-flex align-items-center justify-content-center mb-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 48 48" class="me-2">
                                     <path fill="#ffc107" d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917"/><path fill="#ff3d00" d="m6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691"/><path fill="#4caf50" d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44"/>
                                     <path fill="#1976d2" d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917"/>
